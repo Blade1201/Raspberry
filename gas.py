b33fc947c655f11aa49b3ba11887d25a4c4b8bbf
Roman Holovacki <91392714+Blade1201@users.noreply.github.com>
2022-02-20 13:50:06 +0100

    Málna PC

--- gas.py
+++ gas.py
@@ -1,6 +1,9 @@
 from mq import *
 import RPi.GPIO as GPIO
 import sys, time
+import requests
+import schedule
+from pushbullet import Pushbullet
 mq = MQ();
 
 buzzer = 25
@@ -12,73 +15,65 @@ p = GPIO.PWM(buzzer, 1000)
 p.ChangeDutyCycle(1)
 
 
-channel = 21                   # DO láb GPIO 21, + 5V , GND pedig GND
-GPIO.setup(channel, GPIO.IN)
+pb = Pushbullet("o.dSkeB5qey7pKgy0LgfbovpEQE9uLTddl")
+dev=pb.get_device("Samsung SM-A600FN")
 
-
-def callback(channel):
-    print("\nTűz észlelve!")
-   
+def database():
+    try:
+        connection = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
         
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-GPIO.add_event_callback(channel, callback)
-    
+
+        try:
+        # Tábla létrehozása         # ---------------------- A Tábla létrehozása után be kell kommentelni ezt a részt
+        #    with connection.cursor() as cursor:
+        #        create_table_query = "CREATE TABLE `gases` (`LPG` float DEFAULT NULL," \
+        #                         "`CO` float DEFAULT NULL," \
+        #                         "`Füst` float DEFAULT NULL," \
+        #                         "`Időpont` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp())"
+
+        #        cursor.execute(create_table_query)
+        #        print("Tábla sikeresen létrehozva")
+
+        # Adat behelyezése
+            with connection.cursor() as cursor:
+                insert_query = """INSERT INTO `gases` (LPG, CO, Füst) VALUES (%s, %s, %s)""" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]);
+                cursor.execute(insert_query)
+                connection.commit()
+                
+        finally:
+            connection.close()
+    except Exception as ex:
+        print("Kapcsolat megszakadt...")
+        print(ex)
+
+schedule.every(10).seconds.do(database)
+
+import pymysql
+from config import host,user,password,db_name
+
+
     
 while True:
     perc = mq.MQPercentage()     # Segitség: https://tutorials-raspberrypi.com/configure-and-read-out-the-raspberry-pi-gas-sensor-mq-x/
     sys.stdout.write("\r")
     sys.stdout.write("\033[K")
     sys.stdout.write("LPG: %g ppm, CO: %g ppm, Füst: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
-     
+    schedule.run_pending()
     
     if perc["CO"] > 1:
         GPIO.output(buzzer, GPIO.HIGH)
         p.start(1)
+        push = dev.push_note("Figyelem!!!","Megemelkedett gázszint észlelve!")
+        time.sleep(10)
         
     else:
         p.stop()
         GPIO.output(buzzer, GPIO.LOW)
         
     sys.stdout.flush()
-    time.sleep(0.1)
-    
-
-
-
-import MySQLdb  # MySQL használata MySQLdb könyvtár használatával
-                # Opció 2 -- pymysql könyvtár használata
-
-# Kapcsolat teremtés a connect metódus segitségével
-db = MySQLdb.connect("localhost","felhasznalo","jelszo","adatbazis")
-
-# db objektum létrehozása a cursor() MySQLdb metódusa segitségével
-cursor = db.cursor()
-
-# SQL parancs végrehajtása az execute() metódus segitségével
-cursor.execute("""INSERT INTO gases (LPG, CO, Füst) VALUES (%s, %s, %s) """,( perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
-db.commit()
- 
-time.sleep(60)
-
-
-
-CREATE TABLE `gases` (
-`LPG` FLOAT NULL DEFAULT NULL,
-`CO` FLOAT NULL DEFAULT NULL,
-`Füst` FLOAT NULL DEFAULT NULL,
-`Időpont` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)
-COLLATE='utf8_hungarian_ci'
-ENGINE=InnoDB
-ROW_FORMAT=COMPACT
-;
-
-
-
-# Grafana -- grafikon lekérdezések
-
-SELECT Füst as value, "Füst" as metric, UNIX_TIMESTAMP(Időpont) as time_sec FROM gases WHERE $__timeFilter(Időpont) order by time_sec asc
-
-SELECT LPG as value, "LPG" as metric, UNIX_TIMESTAMP(Időpont) as time_sec FROM gases WHERE $__timeFilter(Időpont) order by time_sec asc
-
-SELECT CO as value, "CO" as metric, UNIX_TIMESTAMP(Időpont) as time_sec FROM gases WHERE $__timeFilter(Időpont) order by time_sec asc
